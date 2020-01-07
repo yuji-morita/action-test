d8002f8b9554ca80251b1e70caad59c6523a8786
yuji-morita <yuji-morita@united.jp>
2020-01-07 18:23:50 +0900

    add script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd `dirname $0`
+mkdir build
+cd build
+
+git clone https://github.com/yuji-morita/action-test.git && cd action-test
+
+mkdocs build -d site
+
+tags=$(git tag)
+for tag in ${tags[@]}
+do
+    git checkout refs/tags/$tag > /dev/null 2>&1
+    mkdocs build -d site/$tag
+done
