15a21f35f6773fef0ce86684a01634aff62320eb
yuji-morita <yuji-morita@united.jp>
2020-01-08 16:03:41 +0900

    fix

--- build.py
+++ build.py
@@ -25,11 +25,11 @@ if tags:
 os.system("mkdocs build -f build/mkdocs.yml -d site")
 
 # versions build
-# if tags:
-#     for tag in tags:
-#         repo.git.checkout(".")
-#         repo.git.checkout(tag)
-#         data["nav"][0]["ガイド"].append({"バージョン": versions})
-#         f = open("build/mkdocs.yml", "w")
-#         yaml.dump(data, f, default_flow_style=False, encoding="utf8", allow_unicode=True)
-#         os.system("mkdocs build -f build/mkdocs.yml -d site/" + tag.name)
+if tags:
+    for tag in tags:
+        repo.git.checkout(".")
+        repo.git.checkout(tag)
+        data["nav"][0]["ガイド"].append({"バージョン": versions})
+        f = open("build/mkdocs.yml", "w")
+        yaml.dump(data, f, default_flow_style=False, encoding="utf8", allow_unicode=True)
+        os.system("mkdocs build -f build/mkdocs.yml -d site/" + tag.name)
